8cac2c9fc593aabb12a97ccef63fd2f292d7ae09
影子和树 <m.hyl@foxmail.com>
2024-10-22 09:59:26 +0800

    关闭自动测试。

--- tests/Inkslab.Linq.Tests/Startup.cs
+++ tests/Inkslab.Linq.Tests/Startup.cs
@@ -13,10 +13,8 @@ namespace Inkslab.Linq.Tests
 
         public override void ConfigureServices(IServiceCollection services, HostBuilderContext context)
         {
-            var connectionStrings = Environment.GetEnvironmentVariable("connectionStrings");
-
             services.UseMySql()
-                .UseLinq(connectionStrings ?? "server=mysql.local.com;uid=root;pwd=yyy@123*;database=framework;AllowLoadLocalInfile=true");
+                .UseLinq("server=mysql.local.com;uid=root;pwd=yyy@123*;database=framework;AllowLoadLocalInfile=true");
 
             services.AddLogging(builder => builder.AddDebug().SetMinimumLevel(LogLevel.Debug));
 
